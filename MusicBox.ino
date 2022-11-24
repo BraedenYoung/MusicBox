cf6e5c1d8499dc1aa3c94dbcbfe0f78fd4863b7a
Beemo <braeden.w.young@gmail.com>
2022-11-23 19:37:33 -0800

    mb : Finish measuring the starting position offsets

--- MusicBox.ino
+++ MusicBox.ino
@@ -1,45 +1,64 @@
 
-  
 #include <Servo.h>
  
-// Define the number of servos
-#define SERVOS 5
-#define SIDE_POS 100
+#define SERVOS 30
+#define DEFAULT_POS 75
  
 // Create the servo object to control a servo.
 Servo myservo[SERVOS];  
  
-// Attach servo to digital pin on the Arduino
-int servo_pins[SERVOS] = {2, 3, 4, 5, 6}; 
-// 
-//// Default angle for the servo in degrees
-//int default_pos[SERVOS] = {0, 0, 0, 0, 0}; 
+// Servos arranged in two rows.
+// One row mirroring the other
+int servo_pins[SERVOS] = {
+  2, 3, 4, 5, 6,
+  7, 8, 9, 10, 11,
+  12, 13, 14, 15, 16,
 
-int offset[SERVOS] = {-3,0,2,5,-8};
+  // Separation
+
+  17, 18, 19, 20, 21,
+  22, 23, 24, 25, 26,
+  27, 28, 29, 30, 31
+}; 
+
+// Measured offset based on the spindle position
+int offset[SERVOS] = {
+  4,  2,  8, -4, -2,
+  6,  6,  8,  -8,  8,
+  12, 4, 12, 1, 4,
+
+  // Separation
+
+  -14, -8, 12, -12, -4,
+  -12, -2, -2, -4, -4, 
+  -10, 2, -2, -2, 6 
+};
  
 void setup() {
    
   for(int i = 0; i < SERVOS; i++) {
-     
     // Attach the servo to the servo object 
     myservo[i].attach(servo_pins[i]);
-   
-    // Make all the servos go to the default position
-    myservo[i].write(0);
+    myservo[i].write(DEFAULT_POS);  
+
+    // wait for servo to get to position before moving on to next
+    delay(100);
   }
-   
-  // Wait 15 milliseconds for the servo to get to the position
-  delay(12);   
 }
   
 void loop() {
-  setAngle(120);
+  setAngle(75);
 }      
 
 void setAngle(int angle) {
    for(int i = 0; i < SERVOS; i++) {
-      myservo[i].write(angle + offset[i]);     
+      myservo[i].write(angle + offset[i]);   
+      delay(100);     
     }           
+}
+
+int getAngleForPin(int pin, int angle, int change) {
+
 }
 
 void sweep() {
