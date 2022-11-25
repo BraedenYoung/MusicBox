2adac47024edb2322c1acd5ef87c8fb94e4ef624
Beemo <braeden.w.young@gmail.com>
2022-11-24 18:56:02 -0800

    mb : Add toneOn and toneOff, start the built-in delay

--- MusicBox.ino
+++ MusicBox.ino
@@ -3,6 +3,8 @@
  
 #define SERVOS 30
 #define DEFAULT_POS 75
+
+#define TONE_HOLD 100
  
 // Create the servo object to control a servo.
 Servo myservo[SERVOS];  
@@ -10,8 +12,8 @@ Servo myservo[SERVOS];
 // Servos arranged in two rows.
 // One row mirroring the other
 int servo_pins[SERVOS] = {
-  2, 3, 4, 5, 6,
-  7, 8, 9, 10, 11,
+   2,  3,  4,  5,  6,
+   7,  8,  9, 10, 11,
   12, 13, 14, 15, 16,
 
   // Separation
@@ -33,65 +35,77 @@ int offset[SERVOS] = {
   -12, -2, -2, -4, -4, 
   -10, 2, -2, -2, 6 
 };
+
+int servoActivationTimer[SERVOS] = {
+  0,0,0,0,0,
+  0,0,0,0,0,
+  0,0,0,0,0,
+
+  0,0,0,0,0,
+  0,0,0,0,0,
+  0,0,0,0,0
+};
  
 void setup() {
    
   for(int i = 0; i < SERVOS; i++) {
     // Attach the servo to the servo object 
     myservo[i].attach(servo_pins[i]);
-    myservo[i].write(DEFAULT_POS);  
-
     // wait for servo to get to position before moving on to next
-    delay(100);
+ 
   }
+  delay(1000);
+  
+  
+  setAllToAngle(75);
+  delay(500);
 }
   
 void loop() {
-  setAngle(75);
+
+  for(int i = 0; i < SERVOS; i++) {
+  delay(500);
+  onTone(i);
+
+  delay(500);
+
+  offTone(i);
+  }
+ 
 }      
 
-void setAngle(int angle) {
+void setAllToAngle(int angle) {
    for(int i = 0; i < SERVOS; i++) {
       myservo[i].write(angle + offset[i]);   
       delay(100);     
     }           
 }
 
-int getAngleForPin(int pin, int angle, int change) {
 
+int checkServoActivationTimer(){
+  for(int i = 0; i < SERVOS; i++) {
+    if (servoActivationTimer[i] == 0) {
+      continue;
+    }
+
+    servoActivationTimer[i] = servoActivationTimer[i] - 1;
+    if (servoActivationTimer[i] == 0) {
+      offTone(i);
+    }    
+  }   
 }
 
-void sweep() {
-    
-  // Go from 0 degrees to 180 degrees
-  // Move in steps of 1 degree
-  for (int angle = 0; angle <= 180; angle += 1) { 
- 
-    // Update the angle for all servos
-    for(int i = 0; i < SERVOS; i++) {
-   
-      // Tell servo to go to the position in variable 'angle'
-      // where 'angle' is in degrees
-      myservo[i].write(angle);  
- 
-      // Wait 15 milliseconds for the servo to get to the position
-      delay(1);    
-    }                   
+void onTone(int index) {
+  int angle = 30;
+  // If the pin is over half way reverse the direction
+  if (SERVOS/2 <= index) {
+    angle = angle * -1;
   }
- 
-  // Go from 180 degrees to 0 degrees
-  // Move in steps of 1 degree   
-  for (int angle = 180; angle >= 0; angle -= 1) { 
- 
-    // Update the angle for all servos
-    for(int i = 0; i < SERVOS; i++) {
-   
-      // Tell servo to go to the position in variable 'angle'
-      // where 'angle' is in degrees
-      myservo[i].write(angle);  
- 
-      // Wait 15 milliseconds for the servo to get to the position
-      delay(12);    
-     }
-  }
+  
+  myservo[index].write(DEFAULT_POS + angle + offset[index]);   
+  // servoActivationTimer[pin] = TONE_HOLD;
+}
+
+void offTone(int index) {
+  myservo[index].write(DEFAULT_POS +  offset[index]);   
 }
